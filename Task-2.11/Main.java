ac36c66dd645b3179d23bea03a1e62d20229d2f3
glew-dev <64249284+glew-dev@users.noreply.github.com>
2020-04-28 00:52:44 +0300

    Create Main.java

---
+++ Task-2.11/Main.java
@@ -0,0 +1,20 @@
+/*
+Задание 2,11: В переменной min лежит число от 0 до 59. Определите в какую четверть часа попадает это число
+ (в первую, вторую, третью или четвертую).
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int min = sc.nextInt();
+        System.out.print("Четверть: ");
+
+        if (min < 15) System.out.println("1");
+        else
+            if (min < 30) System.out.println("2");
+            else
+                if (min < 45) System.out.println("3");
+                else
+                    System.out.println("4");
+    }
+}
