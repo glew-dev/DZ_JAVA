a96c4dc8db89b8ac1c09db2eeb7374ebb47bdf58
glew-dev <64249284+glew-dev@users.noreply.github.com>
2020-04-27 18:29:40 +0300

    Create Main.java

---
+++ Task-1.1-1.6/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+// 1.1
+        int n1 = 5, n2 = 9, n3;
+        n3 = n1 + n2;
+        System.out.println(n3);
+// 1.2
+        double x = 2.5, a, b;
+        a = x * 2; b = x * 3;
+        System.out.println(x + " " + a + " " + b);
+// 1.3
+        int a1 = 4, b1 = 7, c;
+        c = a1 * 10 + b1;
+        System.out.println(c);
+// 1.4
+        int x1 = 30;
+        double xdiv3 = x1 / 3.0;
+        System.out.println(xdiv3);
+// 1.5
+        String name = "Ivan", city = "Mos"; int age = 40;
+        System.out.println(name + " " + city + " " + age);
+// 1.6
+        double number = 2;
+        System.out.println(number * number);
+    }
+}
