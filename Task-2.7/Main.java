bcc5070d252ec5455a7ee969cd5f16c4c15041b6
glew-dev <64249284+glew-dev@users.noreply.github.com>
2020-04-27 22:24:54 +0300

    Create Main.java

---
+++ Task-2.7/Main.java
@@ -0,0 +1,10 @@
+/*
+Задание 2,7: Выведите в консоль столбец чисел от 1 до 50.
+*/
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 51; i++) {
+            System.out.println(i);
+        }
+    }
+    /*
