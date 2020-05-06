e6bb247c132e873a6f56630b07fc1b92d49fb5f0
glew-dev <64249284+glew-dev@users.noreply.github.com>
2020-05-06 17:26:28 +0300

    Update Main.java

--- Task-6.1/Main.java
+++ Task-6.1/Main.java
@@ -6,7 +6,8 @@
 6. Унаследуйте Ferrari и Lanos от CheapCar и LuxuriousCar, подумайте, какой класс для кого.
 7. В классе LuxuriousCar реализуйте метод printlnDesire, чтобы он выводил на экран "Я хочу ездить на роскошной машине".
 8. В классе CheapCar реализуйте метод printlnDesire, чтобы он выводил на экран "Я хочу ездить на дешевой машине".
-9. В классах LuxuriousCar и CheapCar для метода printlnDesire расставьте различными способами модификаторы доступа так, чтобы в классах Ferrari и Lanos выполнялось расширение видимости.*/
+9. В классах LuxuriousCar и CheapCar для метода printlnDesire расставьте различными способами модификаторы доступа
+так, чтобы в классах Ferrari и Lanos выполнялось расширение видимости.*/
 
 public class Main {
     public static void main(String[] args) {
@@ -26,11 +27,13 @@ public class Main {
         }
     }
     public static class Ferrari extends LuxuriousCar {
+        @Override
         public void printlnDesire() {
             System.out.println(WANT_STRING + FERRARI_NAME);
         }
     }
     public static class Lanos extends CheapCar{
+        @Override
         public void printlnDesire() {
             System.out.println(WANT_STRING + LANOS_NAME);
         }
