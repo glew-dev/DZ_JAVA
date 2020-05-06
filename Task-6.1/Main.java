da7771dc2af7441d3081b17bb0ff7291df597cf4
glew-dev <64249284+glew-dev@users.noreply.github.com>
2020-05-06 13:11:18 +0300

    Create Main.java

---
+++ Task-6.1/Main.java
@@ -0,0 +1,45 @@
+/* 1. Для вывода используй только переменные из класса Constants.
+2. В классе Ferrari реализуйте метод printlnDesire, чтобы он выводил на экран "Я хочу ездить на Феррари".
+3. В классе Lanos реализуйте метод printlnDesire, чтобы он выводил на экран "Я хочу ездить на Ланосе".
+4. Создайте public static класс LuxuriousCar(РоскошнаяМашина).
+5. Создайте public static класс CheapCar(ДешеваяМашина).
+6. Унаследуйте Ferrari и Lanos от CheapCar и LuxuriousCar, подумайте, какой класс для кого.
+7. В классе LuxuriousCar реализуйте метод printlnDesire, чтобы он выводил на экран "Я хочу ездить на роскошной машине".
+8. В классе CheapCar реализуйте метод printlnDesire, чтобы он выводил на экран "Я хочу ездить на дешевой машине".
+9. В классах LuxuriousCar и CheapCar для метода printlnDesire расставьте различными способами модификаторы доступа так, чтобы в классах Ferrari и Lanos выполнялось расширение видимости.*/
+
+public class Main {
+    public static void main(String[] args) {
+        new Main.LuxuriousCar().printlnDesire();
+        new Main.CheapCar().printlnDesire();
+        new Main.Ferrari().printlnDesire();
+        new Main.Lanos().printlnDesire();
+    }
+    public static class LuxuriousCar extends Constants {
+        public void printlnDesire() {
+            System.out.println(WANT_STRING + LUXURIOUS_CAR);
+        }
+    }
+    public static class CheapCar extends Constants {
+        public void printlnDesire() {
+            System.out.println(WANT_STRING + CHEAP_CAR);
+        }
+    }
+    public static class Ferrari extends LuxuriousCar {
+        public void printlnDesire() {
+            System.out.println(WANT_STRING + FERRARI_NAME);
+        }
+    }
+    public static class Lanos extends CheapCar{
+        public void printlnDesire() {
+            System.out.println(WANT_STRING + LANOS_NAME);
+        }
+    }
+    public static class Constants {
+        public static String WANT_STRING = "Я хочу ездить на ";
+        public static String LUXURIOUS_CAR = "роскошной машине";
+        public static String CHEAP_CAR = "дешевой машине";
+        public static String FERRARI_NAME = "Феррари";
+        public static String LANOS_NAME = "Ланосе";
+    }
+}
