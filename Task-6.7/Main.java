0f323d42bfd51947078eb38646d25c9e6de27853
glew-dev <64249284+glew-dev@users.noreply.github.com>
2020-05-07 13:00:49 +0300

    Create Main.java

---
+++ Task-6.7/Main.java
@@ -0,0 +1,15 @@
+/* Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
+String s = null;
+String m = s.toLowerCase(); */
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String s = null;
+            String m = s.toLowerCase();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Программа завершена");
+    }
+}
